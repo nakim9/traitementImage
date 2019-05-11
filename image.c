c497b3371d019dc1f005bbb06b65db186ca0c278
callo13 <49616412+callo13@users.noreply.github.com>
2019-05-11 15:22:37 +0200

    Generation image

--- image.c
+++ image.c
@@ -4,6 +4,389 @@
 
 
 
+IMAGE * creer3MatricesDegrade(DonneesImageRGB *image)
+{
+	/* ---Variables--- */
+	int hauteurImage = image->hauteurImage;
+	int largeurImage = image->largeurImage;
+	short ** bleu 	= NULL;
+	short ** vert 	= NULL;
+	short ** rouge 	= NULL;
+	int r=0;
+
+	/* ---Allocation--- */
+	bleu	= (short **)malloc(sizeof(short *) * hauteurImage);
+	vert	= (short **)malloc(sizeof(short *) * hauteurImage);
+	rouge	= (short **)malloc(sizeof(short *) * hauteurImage);
+
+	if(bleu == NULL || vert == NULL || rouge == NULL)
+	{
+		printf("Error malloc\n");
+		return NULL;
+	}
+
+	for(int i=0 ; i < hauteurImage ; i++)
+	{
+		bleu[i]	= NULL;
+		vert[i]	= NULL;
+		rouge[i]	= NULL;
+
+		bleu[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		vert[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		rouge[i]	= (short *)malloc(sizeof(short) * largeurImage);
+
+		if(bleu[i] == NULL || vert[i] == NULL || rouge[i] == NULL)
+		{
+			printf("Error malloc\n");
+			return NULL;
+		}
+	}
+
+	/* ---Insertion dans les matrices--- */
+	for(int i=0 ; i < hauteurImage ; i++)
+	{
+		for(int j=0 ; j < largeurImage ; j++)
+		{
+			short posB = 0;
+			short posV = 1;
+			short posR = 2;
+
+			bleu[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posB];
+			vert[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posV];
+			rouge[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posR];
+
+			if(j!=255)
+            {
+                bleu[i][j]=255-r;
+                rouge[i][j]=255-r;
+                vert[i][j]=255-r;
+            }
+            else
+            {
+                bleu[i][j]=255-r;
+                rouge[i][j]=255-r;
+                vert[i][j]=255-r;
+                r++;
+            }
+
+
+		}
+	}
+
+	IMAGE * couleur = NULL;
+	couleur = malloc(sizeof(IMAGE));
+
+	couleur->hauteurImage = hauteurImage;
+	couleur->largeurImage = largeurImage;
+	couleur->Bleu = bleu;
+	couleur->Vert = vert;
+	couleur->Rouge = rouge;
+
+	return couleur;
+}
+
+
+IMAGE * creer3MatricesDamier(DonneesImageRGB *image)
+{
+	/* ---Variables--- */
+	int hauteurImage = image->hauteurImage;
+	int largeurImage = image->largeurImage;
+	short ** bleu 	= NULL;
+	short ** vert 	= NULL;
+	short ** rouge 	= NULL;
+	short ** nb = NULL;
+
+	/* ---Allocation--- */
+	bleu	= (short **)malloc(sizeof(short *) * hauteurImage);
+	vert	= (short **)malloc(sizeof(short *) * hauteurImage);
+	rouge	= (short **)malloc(sizeof(short *) * hauteurImage);
+
+	if(bleu == NULL || vert == NULL || rouge == NULL)
+	{
+		printf("Error malloc\n");
+		return NULL;
+	}
+
+	for(int i=0 ; i < hauteurImage ; i++)
+	{
+		bleu[i]	= NULL;
+		vert[i]	= NULL;
+		rouge[i]	= NULL;
+
+		bleu[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		vert[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		rouge[i]	= (short *)malloc(sizeof(short) * largeurImage);
+
+		if(bleu[i] == NULL || vert[i] == NULL || rouge[i] == NULL)
+		{
+			printf("Error malloc\n");
+			return NULL;
+		}
+	}
+
+	/* ---Insertion dans les matrices--- */
+	for(int i=0 ; i < hauteurImage ; i++)
+	{
+		for(int j=0 ; j < largeurImage ; j++)
+		{
+			short posB = 0;
+			short posV = 1;
+			short posR = 2;
+
+			bleu[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posB];
+			vert[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posV];
+			rouge[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posR];
+
+			if(((i/64)+(j/64))%2)
+            {
+                bleu[i][j]=255;
+                rouge[i][j]=255;
+                vert[i][j]=255;
+            }
+            else
+            {
+               bleu[i][j]=0;
+                rouge[i][j]=0;
+                vert[i][j]=0;
+            }
+
+
+		}
+	}
+
+	IMAGE * couleur = NULL;
+	couleur = malloc(sizeof(IMAGE));
+
+	couleur->hauteurImage = hauteurImage;
+	couleur->largeurImage = largeurImage;
+	couleur->Bleu = bleu;
+	couleur->Vert = vert;
+	couleur->Rouge = rouge;
+
+	return couleur;
+}
+
+
+
+IMAGE * creer3MatricesSeuillage(DonneesImageRGB *image)
+{
+	/* ---Variables--- */
+	int hauteurImage = image->hauteurImage;
+	int largeurImage = image->largeurImage;
+	short ** bleu 	= NULL;
+	short ** vert 	= NULL;
+	short ** rouge 	= NULL;
+	short ** gris   = NULL;
+
+	/* ---Allocation--- */
+	bleu	= (short **)malloc(sizeof(short *) * hauteurImage);
+	vert	= (short **)malloc(sizeof(short *) * hauteurImage);
+	rouge	= (short **)malloc(sizeof(short *) * hauteurImage);
+	gris	= (short **)malloc(sizeof(short *) * hauteurImage);
+
+	if(bleu == NULL || vert == NULL || rouge == NULL || gris == NULL)
+	{
+		printf("Error malloc\n");
+		return NULL;
+	}
+
+	for(int i=0 ; i < hauteurImage ; i++)
+	{
+		bleu[i]	= NULL;
+		vert[i]	= NULL;
+		rouge[i]	= NULL;
+		gris[i]	= NULL;
+
+		bleu[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		vert[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		rouge[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		gris[i]	= (short *)malloc(sizeof(short) * largeurImage);
+
+		if(bleu[i] == NULL || vert[i] == NULL || rouge[i] == NULL || gris[i] == NULL)
+		{
+			printf("Error malloc\n");
+			return NULL;
+		}
+	}
+
+	/* ---Insertion dans les matrices--- */
+	for(int i=0 ; i < hauteurImage ; i++)
+	{
+		for(int j=0 ; j < largeurImage ; j++)
+		{
+			short posB = 0;
+			short posV = 1;
+			short posR = 2;
+
+			bleu[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posB];
+			vert[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posV];
+			rouge[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posR];
+			gris[i][j] = (rouge[i][j]+vert[i][j]+rouge[i][j])/3;
+
+			if(gris[i][j] <= 128)
+            {
+                gris[i][j]=0;
+            }
+            else
+            {
+                gris[i][j]=255;
+            }
+		}
+	}
+
+	IMAGE * couleur = NULL;
+	couleur = malloc(sizeof(IMAGE));
+
+	couleur->hauteurImage = hauteurImage;
+	couleur->largeurImage = largeurImage;
+	couleur->Bleu = gris;
+	couleur->Vert = gris;
+	couleur->Rouge = gris;
+
+	return couleur;
+}
+
+
+
+
+
+IMAGE * creer3MatricesNuancedegris(DonneesImageRGB *image)
+{
+	/* ---Variables--- */
+	int hauteurImage = image->hauteurImage;
+	int largeurImage = image->largeurImage;
+	short ** bleu 	= NULL;
+	short ** vert 	= NULL;
+	short ** rouge 	= NULL;
+	short ** gris   = NULL;
+
+	/* ---Allocation--- */
+	bleu	= (short **)malloc(sizeof(short *) * hauteurImage);
+	vert	= (short **)malloc(sizeof(short *) * hauteurImage);
+	rouge	= (short **)malloc(sizeof(short *) * hauteurImage);
+	gris	= (short **)malloc(sizeof(short *) * hauteurImage);
+
+	if(bleu == NULL || vert == NULL || rouge == NULL || gris == NULL)
+	{
+		printf("Error malloc\n");
+		return NULL;
+	}
+
+	for(int i=0 ; i < hauteurImage ; i++)
+	{
+		bleu[i]	= NULL;
+		vert[i]	= NULL;
+		rouge[i]	= NULL;
+		gris[i]	= NULL;
+
+		bleu[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		vert[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		rouge[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		gris[i]	= (short *)malloc(sizeof(short) * largeurImage);
+
+		if(bleu[i] == NULL || vert[i] == NULL || rouge[i] == NULL || gris[i] == NULL)
+		{
+			printf("Error malloc\n");
+			return NULL;
+		}
+	}
+
+	/* ---Insertion dans les matrices--- */
+	for(int i=0 ; i < hauteurImage ; i++)
+	{
+		for(int j=0 ; j < largeurImage ; j++)
+		{
+			short posB = 0;
+			short posV = 1;
+			short posR = 2;
+
+			bleu[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posB];
+			vert[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posV];
+			rouge[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posR];
+			gris[i][j] = (rouge[i][j]+vert[i][j]+rouge[i][j])/3;
+		}
+	}
+
+	IMAGE * couleur = NULL;
+	couleur = malloc(sizeof(IMAGE));
+
+	couleur->hauteurImage = hauteurImage;
+	couleur->largeurImage = largeurImage;
+	couleur->Bleu = gris;
+	couleur->Vert = gris;
+	couleur->Rouge = gris;
+
+	return couleur;
+}
+
+
+
+
+IMAGE * creer3MatricesNormal(DonneesImageRGB *image)
+{
+	/* ---Variables--- */
+	int hauteurImage = image->hauteurImage;
+	int largeurImage = image->largeurImage;
+	short ** bleu 	= NULL;
+	short ** vert 	= NULL;
+	short ** rouge 	= NULL;
+
+	/* ---Allocation--- */
+	bleu	= (short **)malloc(sizeof(short *) * hauteurImage);
+	vert	= (short **)malloc(sizeof(short *) * hauteurImage);
+	rouge	= (short **)malloc(sizeof(short *) * hauteurImage);
+
+	if(bleu == NULL || vert == NULL || rouge == NULL)
+	{
+		printf("Error malloc\n");
+		return NULL;
+	}
+
+	for(int i=0 ; i < hauteurImage ; i++)
+	{
+		bleu[i]	= NULL;
+		vert[i]	= NULL;
+		rouge[i]	= NULL;
+
+		bleu[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		vert[i]	= (short *)malloc(sizeof(short) * largeurImage);
+		rouge[i]	= (short *)malloc(sizeof(short) * largeurImage);
+
+		if(bleu[i] == NULL || vert[i] == NULL || rouge[i] == NULL)
+		{
+			printf("Error malloc\n");
+			return NULL;
+		}
+	}
+
+	/* ---Insertion dans les matrices--- */
+	for(int i=0 ; i < hauteurImage ; i++)
+	{
+		for(int j=0 ; j < largeurImage ; j++)
+		{
+			short posB = 0;
+			short posV = 1;
+			short posR = 2;
+
+			bleu[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posB];
+			vert[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posV];
+			rouge[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posR];
+
+		}
+	}
+
+	IMAGE * couleur = NULL;
+	couleur = malloc(sizeof(IMAGE));
+
+	couleur->hauteurImage = hauteurImage;
+	couleur->largeurImage = largeurImage;
+	couleur->Bleu = bleu;
+	couleur->Vert = vert;
+	couleur->Rouge = rouge;
+
+	return couleur;
+}
+
 
 IMAGE * creer3Matrices(DonneesImageRGB *image)
 {
@@ -54,6 +437,9 @@ IMAGE * creer3Matrices(DonneesImageRGB *image)
 			bleu[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posB];
 			vert[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posV];
 			rouge[i][j] = image->donneesRGB[i*largeurImage*3 + j*3 + posR];
+			bleu[i][j]=255-bleu[i][j];
+			vert[i][j]=255-vert[i][j];
+			rouge[i][j]=255-rouge[i][j];
 		}
 	}
 
--- main.c
+++ main.c
@@ -5,14 +5,75 @@
 
 int main(void)
 {
+    int choix;
 	DonneesImageRGB *image = NULL;
+	DonneesImageRGB *seuillage = NULL;
 	image = lisBMPRGB("hibou.bmp");
+	seuillage = lisBMPRGB("Seuillage.bmp");
+	IMAGE * matrices=NULL;
 
-	IMAGE * matrices = creer3Matrices(image);
+	while(choix=!0)
+    {
 
-	image = creerImage(matrices);
+	printf("Traitement d'images\n");
+	printf("1 - Negatif d'une image\n");
+	printf("2 - Transformation en niveaux de gris d'un image couleur\n");
+	printf("3 - Seuillage\n");
+	printf("4 - Afficher image originale\n");
+	printf("5 - Afficher image damier\n");
+	printf("6 - Afficher image degrade\n");
+	printf("0 - Quitter\n");
+	scanf("%d",&choix);
 
-	ecrisBMPRGB_Dans(image,"test2.bmp");
+	switch(choix)
+	{
+        case 1:
+            matrices = creer3Matrices(image);
+            image = creerImage(matrices);
+            ecrisBMPRGB_Dans(image,"Negatif.bmp");
+            printf("Fichier copie sous le nom de Negatif\n");
+            break;
+        case 4:
+            matrices = creer3MatricesNormal(image);
+            image = creerImage(matrices);
+            ecrisBMPRGB_Dans(image,"Hibou copie.bmp");
+            printf("Fichier copie sous le nom de Hibou copie\n");
+            break;
+        case 2:
+            matrices = creer3MatricesNuancedegris(image);
+            image = creerImage(matrices);
+            ecrisBMPRGB_Dans(image,"Nuance de gris.bmp");
+            printf("Fichier copie sous le nom de Nuance de gris\n");
+            break;
+        case 3:
+            matrices = creer3MatricesSeuillage(image);
+            image = creerImage(matrices);
+            ecrisBMPRGB_Dans(image,"Seuillage.bmp");
+            printf("Fichier copie sous le nom de Seuillage\n");
+            break;
+        case 5:
+            matrices = creer3MatricesDamier(image);
+            image = creerImage(matrices);
+            ecrisBMPRGB_Dans(image,"Damier.bmp");
+            printf("Fichier copie sous le nom de Damier\n");
+            break;
+         case 6:
+            matrices = creer3MatricesDegrade(image);
+            image = creerImage(matrices);
+            ecrisBMPRGB_Dans(image,"Degrade.bmp");
+            printf("Fichier copie sous le nom de Degrade\n");
+            break;
+        case 0:
+            printf("Bye Bye \n");
+            return 0;
+            break;
+
+
+
+	}
+    }
+
 
 	return 0;
 }
+
